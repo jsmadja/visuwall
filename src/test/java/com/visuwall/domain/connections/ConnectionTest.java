ea7766bc633405413c557aa2a03332e68cef2776
Julien Smadja <jsmadja@xebia.fr>
2012-12-16 11:58:41 +0100

    Correction compilation

--- src/test/java/com/visuwall/domain/connections/ConnectionTest.java
+++ src/test/java/com/visuwall/domain/connections/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.visuwall.domain.connections;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -10,53 +11,53 @@ public class ConnectionTest {
     @Test
     public void should_accept_any_names_when_there_is_no_build_filter() throws Exception {
         Connection connectionConfiguration = new Connection();
-        Assert.assertTrue(connectionConfiguration.accept("mybuild"));
+        assertTrue(connectionConfiguration.accept("mybuild"));
     }
 
     @Test
     public void should_accept_any_names_starting_with_A() throws Exception {
         Connection connectionConfiguration = new Connection();
         connectionConfiguration.setBuildFilter("regexp:A.*");
-        Assert.assertTrue(connectionConfiguration.accept("Azerty"));
-        Assert.assertFalse(connectionConfiguration.accept("bzerty"));
+        assertTrue(connectionConfiguration.accept("Azerty"));
+        assertFalse(connectionConfiguration.accept("bzerty"));
     }
 
     @Test
     public void should_accept_any_names_starting_with_a() throws Exception {
         Connection connectionConfiguration = new Connection();
         connectionConfiguration.setBuildFilter("regexp:a.*");
-        Assert.assertTrue(connectionConfiguration.accept("azerty"));
-        Assert.assertFalse(connectionConfiguration.accept("bzerty"));
+        assertTrue(connectionConfiguration.accept("azerty"));
+        assertFalse(connectionConfiguration.accept("bzerty"));
     }
 
     @Test
     public void should_accept_any_names_starting_with_a_or_A() throws Exception {
         Connection connectionConfiguration = new Connection();
         connectionConfiguration.setBuildFilter("regexp:(?i)a.*");
-        Assert.assertTrue(connectionConfiguration.accept("azerty"));
-        Assert.assertTrue(connectionConfiguration.accept("Azerty"));
-        Assert.assertFalse(connectionConfiguration.accept("bzerty"));
+        assertTrue(connectionConfiguration.accept("azerty"));
+        assertTrue(connectionConfiguration.accept("Azerty"));
+        assertFalse(connectionConfiguration.accept("bzerty"));
     }
 
     @Test
     public void should_accept_any_names_containing_with_ert() throws Exception {
         Connection connectionConfiguration = new Connection();
         connectionConfiguration.setBuildFilter("regexp:.*ert.*");
-        Assert.assertTrue(connectionConfiguration.accept("azerty"));
-        Assert.assertTrue(connectionConfiguration.accept("Azerty"));
-        Assert.assertFalse(connectionConfiguration.accept("bzeprty"));
+        assertTrue(connectionConfiguration.accept("azerty"));
+        assertTrue(connectionConfiguration.accept("Azerty"));
+        assertFalse(connectionConfiguration.accept("bzeprty"));
     }
 
     @Test
     public void should_accept_any_names_containing_with_names_split_by_comma() throws Exception {
         Connection connectionConfiguration = new Connection();
         connectionConfiguration.setBuildFilter("cautions*,fxent,Hermes");
-        Assert.assertTrue(connectionConfiguration.accept("cautions"));
-        Assert.assertTrue(connectionConfiguration.accept("fxent"));
-        Assert.assertFalse(connectionConfiguration.accept("fxent-it"));
-        Assert.assertTrue(connectionConfiguration.accept("cautions-it"));
-        Assert.assertFalse(connectionConfiguration.accept("bzeprty"));
-        Assert.assertTrue(connectionConfiguration.accept("hermes"));
+        assertTrue(connectionConfiguration.accept("cautions"));
+        assertTrue(connectionConfiguration.accept("fxent"));
+        assertFalse(connectionConfiguration.accept("fxent-it"));
+        assertTrue(connectionConfiguration.accept("cautions-it"));
+        assertFalse(connectionConfiguration.accept("bzeprty"));
+        assertTrue(connectionConfiguration.accept("hermes"));
     }
 
 }
